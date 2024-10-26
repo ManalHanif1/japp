f4c396a38d9827caa87b87477cfc1f3b4ebcf122
ManalHanif1 <devlopeerm@gmail.com>
2024-10-26 22:43:48 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.write('hello')
+import pandas as pd
+
+data = {
+    'Fruit': ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'],
+    'Quantity': [10, 15, 20, 25, 30],
+    'Price': [0.5, 0.25, 0.75, 1.0, 2.0]
+}
+df = pd.DataFrame(data)
+st.dataframe(df, height=300)
